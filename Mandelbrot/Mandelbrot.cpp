5ec3bd326a4bd0cc332a5fa41471f1c2127b98d0
lor saetern <lor.saetern89@gmail.com>
2024-04-23 14:57:12 -0700

    update mandelbrot

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -1,1 +1,85 @@
-//Test
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <complex>
+#include <cmath>
+#include "ComplexPlane.h"
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	int pixelWidth = VideoMode::getDesktopMode().width / 2;
+	int pixelHeight = VideoMode::getDesktopMode().height / 2;
+	VideoMode vm(pixelWidth, pixelHeight);
+	RenderWindow window(vm, "Mandelbrot", Style::Default);
+
+	//View view;
+	////view.setSize(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
+	////view.setCenter(VideoMode::getDesktopMode().width / 2, VideoMode::getDesktopMode().height / 2);
+	//view.setSize(pixelWidth, pixelHeight);
+	//view.setCenter(pixelWidth / 2, pixelHeight / 2);
+	//window.setView(view);
+
+	ComplexPlane complexPlane(pixelWidth, pixelHeight);
+
+	//Text
+	Font font;
+	if (!font.loadFromFile("Gundam.otf"))
+	{
+		cout << "Can't load the font file." << endl;
+	}
+	Text text1("TEST", font);
+	text1.setCharacterSize(20);
+	text1.setStyle(Text::Regular);
+	text1.setFillColor(Color::White);
+
+	while (window.isOpen())
+	{
+		//User input
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) window.close();
+			else if (event.type == Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == Mouse::Right)
+				{
+					cout << "the right button was pressed" << endl;
+					cout << "mouse x: " << event.mouseButton.x << endl;
+					cout << "mouse y: " << event.mouseButton.y << endl;
+					complexPlane.zoomOut();
+					complexPlane.setCenter(Mouse::getPosition(window));
+				}
+				else if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					cout << "the left button was pressed" << endl;
+					cout << "mouse x: " << event.mouseButton.x << endl;
+					cout << "mouse y: " << event.mouseButton.y << endl;
+					complexPlane.zoomIn();
+					complexPlane.setCenter(Mouse::getPosition(window));
+				}
+			}
+			if (event.type == sf::Event::MouseMoved)
+			{
+				complexPlane.setMouseLocation(Mouse::getPosition(window));
+			}
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		complexPlane.updateRender();
+		complexPlane.loadText(text1);
+
+		//Draw
+		window.clear();
+		window.draw(complexPlane);
+		window.draw(text1);
+		window.display();
+	}
+}
